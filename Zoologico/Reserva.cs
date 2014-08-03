03792e1c27d3bb6506ee7a06a6f854f5126cc74c
Rut Jeronimo <rjb.jeronimo@gmail.com>
2014-08-03 11:46:02 -0500

    comiteando Testing proyect de los unit tests y Test results

---
+++ Zoologico/Reserva.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zoologico
+{
+    class Reserva
+    {
+        public int CodigoReserva { get; set; }
+        public int CodigoUsuario { get; set; }
+        public int Asistentes { get; set; }
+        public DateTime Fecha_reserva { get; set; }
+        public string Turno { get; set; }
+        public string Preferencias { get; set; }
+    }
+}
